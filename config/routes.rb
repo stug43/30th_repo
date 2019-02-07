b5f245e0b8dfc84b0be477170b8529add6aa0a15
stug43 <nganta.a@hotmail.fr>
2019-02-08 00:03:12 +0100

    correcting shit merge

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-<<<<<<< HEAD
   get 'cities/show'
   get 'cities/index'
   resources :users
@@ -16,17 +15,4 @@ Rails.application.routes.draw do
 	get '/search', to: 'gossips#search'
 	post '/search', to: 'gossips#search_post'
 	get '/error_404', to: 'static_pages#error'
-=======
-  resources :gossips do
-    resources :comments, only: %i[new create index destroy]
-  end
-  root to: 'gossips#index'
-
-  get '/contact', to: 'static_pages#contact'
-  get '/team', to: 'static_pages#team'
-  get '/search', to: 'gossips#search'
-  post '/search', to: 'gossips#search_post'
-  get '/gossips/:id', to: 'gossips#show'
-  get '/error_404', to: 'static_pages#error'
->>>>>>> 510d0ba761c930da3c4c651128718697155a5860
 end
